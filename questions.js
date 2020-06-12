3efcb664be058b2864f2734f872d412284d0605a
Tripti Shres <tripti.shres@gmail.com>
2020-06-12 21:39:31 +1000

    Created questions for quiz

---
+++ questions.js
@@ -0,0 +1,38 @@
+const questions = [{
+        title: "What is the name of the man Rachel almost marries?",
+        choices: ["Ricky", "Sam", "Berry", "Jerry"],
+        answer: "Berry"
+    },
+
+    {
+        title: "In Friends, how many times has Ross been married??",
+        choices: ["Only once", "3 times", "2 times", "4 times"],
+        answer: "3 times"
+    },
+
+    {
+        title: "What is the name of Ross' first wife?",
+        choices: ["Rachel", "Carol", "Emily", "Susie"],
+        answer: "Carol"
+    },
+
+    {
+        title: "What is the name of the Central Perk employee who's in love with Rachel?",
+        choices: ["Gunther", "Maxwell", "Louis", "George"],
+        answer: "Gunther"
+    },
+
+    {
+        title: "What do they call their neighbor across the street??",
+        choices: ["Naked Guy", "Ugly Naked Guy", "The Poker", "Naked Neighbour Guy"],
+        answer: "Cantharellus"
+    },
+
+    {
+        title: "Who finds out about Monica and Chandler first?",
+        choices: ["Ross", "Rachel", "Joey", "Phoebe"],
+        answer: "Joey"
+    },
+
+
+];
